64aae3dc6d3e572c3a759140d3ff0488471bc5fd
Tiffany Kyi <tiffanykyi@gmail.com>
2017-03-03 18:18:21 -0800

    Add public ssh key to Heroku

--- bin/heroku_deploy.sh
+++ bin/heroku_deploy.sh
@@ -4,12 +4,18 @@ set -e
 
 HEROKUCLI_DIR=/usr/bin
 
-echo "Which git"
-which git
+echo `User is ${USER}`
 
-# echo "Check Heroku version.."
-# ${HEROKUCLI_DIR}/heroku create
+echo "Put public ssh key in /Users/${USER}/.ssh/id_rsa.pub.."
+mkdir -p /Users/${USER}/.ssh
+echo `${PUBLIC_SSH_KEY}` >> /Users/${USER}/.ssh/id_rsa.pub
+cat /Users/${USER}/.ssh/id_rsa.pub
 
+echo "Add key to Heroku.."
+${HEROKUCLI_DIR}/heroku keys:add
 
-# echo "Deploy Heroku app.."
-# git push heroku heroku:master
+echo "Create Heroku app.."
+${HEROKUCLI_DIR}/heroku create
+
+echo "Deploy Heroku app.."
+git push heroku heroku:master
