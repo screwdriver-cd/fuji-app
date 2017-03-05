2a647dc17cd67ed2f4e26a4c9c215180ece08656
Tiffany Kyi <tiffanykyi@gmail.com>
2017-03-04 23:07:09 -0800

    Add public SSH key

--- bin/heroku_deploy.sh
+++ bin/heroku_deploy.sh
@@ -23,6 +23,11 @@ echo "Adding heroku ssh fingerprint.."
 mkdir -p ~/.ssh
 ssh-keyscan -H heroku.com > ~/.ssh/known_hosts 
 
+echo "Adding public SSH key.."
+cat >~/.ssh/id_rsa.pub <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCWPgMa6idxP27UT1jrcdFbjyDEj7+AQjAsn86+F5qDw1oU7pxPMFNsC1L2cIaxRo0XKJ3sxrs3cl/Jr8oc6SQoCWaIdHKpcgf7uo2yQb9ZshrVX9FRt0lBOmVCcc/E3rzzSGZNIecY1Sg+JwzsUvs9syiv9veYCVRKljyoIA3o5p8dXpNvv1H9+xdg2XuHOehLLbqnBp79LnnKUbVgQ+Uhp5UEkAEo6Wv5jYWLlifQMKVDD06Ss18Ub0I9B3XcCc19vx4y6p0QsVuUBR4ZUxzw1/yA+xlpvZ+BHskraKclbYWaWYIVbcLv9pLRj6tRW7lDsvndWuJxmjPCm+SjdZ07 tkyi@shoulderwork-lm
+EOF
+
 echo "Verifying with DNS.."
 cat >~/.ssh/config <<EOF
 Host heroku.com
