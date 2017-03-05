a0afd4f477f5489e8ddc8a274c040f3f1bdeea16
Tiffany Kyi <tiffanykyi@gmail.com>
2017-03-04 22:07:43 -0800

    Push to Heroku app; add Heroku to known hosts

--- bin/heroku_deploy.sh
+++ bin/heroku_deploy.sh
@@ -5,17 +5,29 @@ set -e
 HEROKUCLI_DIR=/usr/bin
 
 echo "Login to Heroku.."
-
 cat >~/.netrc <<EOF
 machine api.heroku.com
-  login $HEROKU_EMAIL
-  password $HEROKU_TOKEN
+  login "${HEROKU_EMAIL}"
+  password "${HEROKU_TOKEN}"
 machine git.heroku.com
-  login $HEROKU_EMAIL
-  password $HEROKU_TOKEN
+  login "${HEROKU_EMAIL}"
+  password "${HEROKU_TOKEN}"
 EOF
 
 chmod 600 ~/.netrc
+
+echo "Add the remote.."
+git remote add heroku "git@heroku.com:${HEROKU_APP}.git"
+
+echo "Adding heroku ssh fingerprint.."
+mkdir -p ~/.ssh
+ssh-keyscan -H heroku.com > ~/.ssh/known_hosts 
+
+echo "Verifying with DNS.."
+cat >~/.ssh/config <<EOF
+Host heroku.com
+ VerifyHostKeyDNS yes
+EOF
 
 echo "Deploy Heroku app.."
 git push heroku heroku:master
